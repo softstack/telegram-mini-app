46f330d4ade3d60e8eb5d9af164762007186f6c6
daniel-vahn <d.vonahn@chainsulting.de>
2024-03-27 10:14:09 +0100

    initial commit of vite project

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import basicSsl from '@vitejs/plugin-basic-ssl';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react(), basicSsl()],
+    build: {
+        outDir: './docs',
+    },
+    base: './',
+});
