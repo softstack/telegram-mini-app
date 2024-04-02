fc70657b7b795b3dec51fce23e5c4241336e3959
daniel-vahn <d.vonahn@chainsulting.de>
2024-04-02 13:26:49 +0200

    cleanup

--- src/components/connectors/WalletConnectModal.tsx
+++ src/components/connectors/WalletConnectModal.tsx
@@ -6,21 +6,13 @@ import { useWeb3Modal } from '@web3modal/ethers/react';
 type Props = {
     title: string;
     icon: string;
-    // callback: (promise: Promise<string>) => void;
 };
 
 const WalletConnectModal: React.FC<Props> = ({ title, icon }) => {
     const { open } = useWeb3Modal();
 
     const openModal = () => {
-        const promise = open({ view: 'Connect' });
-
-        if (promise) {
-            console.log(promise);
-            promise.then((result) => {
-                console.log(result);
-            });
-        }
+        open({ view: 'Connect' });
     };
 
     return (
