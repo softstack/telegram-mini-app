336edce49d626857b0e9c45563e92615e126ce84
daniel-vahn <d.vonahn@chainsulting.de>
2024-04-10 13:08:54 +0200

    wc universal link

--- src/components/connectors/WalletConnectModal.tsx
+++ src/components/connectors/WalletConnectModal.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
-import { useWeb3Modal } from '@web3modal/ethers/react';
+import { EthereumProvider } from '@walletconnect/ethereum-provider';
+import WebApp from '@twa-dev/sdk';
+
+// import WalletConnect from '@walletconnect/client';
+// import { useWeb3Modal } from '@web3modal/ethers/react';
 import ConnectButton from '../buttons/ConnectButton';
 
 type Props = {
@@ -9,10 +13,40 @@ type Props = {
 };
 
 const WalletConnectModal: React.FC<Props> = ({ title, icon }) => {
-    const { open } = useWeb3Modal();
+    // const [provider, setProvider] = useState<any>(null);
+
+    const PROJECT_ID = import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID || '';
+
+    const init = async () => {
+        const provider = await EthereumProvider.init({
+            projectId: PROJECT_ID,
+            metadata: {
+                name: 'TMA Wallet PoC',
+                description: 'Connect your wallet to a telegram mini app',
+                url: 'https://softstackhq.github.io/telegram-mini-app/', // origin must match your domain & subdomain
+                icons: ['https://avatars.githubusercontent.com/u/37784886'],
+            },
+            showQrModal: false,
+            optionalChains: [1, 137, 2020],
+
+            /*Optional - Add custom RPCs for each supported chain*/
+            rpcMap: {
+                1: 'mainnet.rpc...',
+                137: 'polygon.rpc...',
+            },
+        });
+        provider.on('display_uri', handleURI);
+        await provider.connect();
+    };
+
+    const handleURI = async (uri: string) => {
+        console.log('uri', uri);
+        const universalLink = `https://mywallet.com/wc?uri=${uri}`;
+        WebApp.openLink(universalLink);
+    };
 
     const openModal = () => {
-        open();
+        init();
     };
 
     return (
