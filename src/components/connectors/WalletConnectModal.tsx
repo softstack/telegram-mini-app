00ff255ec35e2865356d4e9b63661d241936bfcb
daniel-vahn <d.vonahn@chainsulting.de>
2024-04-15 12:10:16 +0200

    bridge first test

--- src/components/connectors/WalletConnectModal.tsx
+++ src/components/connectors/WalletConnectModal.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-
-import { EthereumProvider } from '@walletconnect/ethereum-provider';
-import WebApp from '@twa-dev/sdk';
+import axios from 'axios';
+// import WebApp from '@twa-dev/sdk';
 
 import ConnectButton from '../buttons/ConnectButton';
-import PrimaryButton from '../buttons/PrimaryButton';
 
 type Props = {
     title: string;
@@ -12,73 +10,26 @@ type Props = {
     accountCallback: (account: string | null) => void;
 };
 
-const WalletConnectModal: React.FC<Props> = ({
-    title,
-    icon,
-    accountCallback,
-}) => {
-    const PROJECT_ID = import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID || '';
-
-    const [provider, setProvider] = React.useState<any | null>(null);
+const WalletConnectModal: React.FC<Props> = ({ title, icon }) => {
     const [uri, setUri] = React.useState<string | null>(null);
 
-    React.useEffect(() => {
-        const init = async () => {
-            try {
-                const provider = await EthereumProvider.init({
-                    projectId: PROJECT_ID,
-                    metadata: {
-                        name: 'TMA Wallet PoC',
-                        description:
-                            'Connect your wallet to a telegram mini app',
-                        url: 'https://softstackhq.github.io/telegram-mini-app/', // origin must match your domain & subdomain
-                        icons: [
-                            'https://avatars.githubusercontent.com/u/37784886',
-                        ],
-                    },
-                    showQrModal: false,
-                    optionalChains: [1, 137, 2020],
-                });
-
-                setProvider(provider);
-                accountCallback(provider.accounts[0]);
-                provider.on('display_uri', handleURI);
-                await provider.connect();
-            } catch (_: unknown) {
-                console.error("Couldn't connect");
-            }
-        };
-        init();
-    }, []);
-
-    const handleURI = async (uri: string) => {
-        setUri(uri);
-    };
-
     const connect = async () => {
-        if (!uri) {
-            return;
-        }
-        const encodedUri = encodeURIComponent(uri);
-        const universalLink = `https://metamask.app.link/wc?uri=${encodedUri}`;
-        WebApp.openLink(universalLink);
-    };
-
-    const handleDisconnect = async () => {
-        if (provider) {
-            await provider.disconnect();
-            accountCallback(null);
-            setProvider(null);
-        }
+        // Post request to localhost:3000/connect with axios
+        axios
+            .post(
+                'https://8d58-2a02-8106-21-bc00-20a7-e089-bdb8-452a.ngrok-free.app/connect'
+            )
+            .then((response) => {
+                // Set uri with response data
+                console.log(response.data);
+                setUri(response.data.universalLink);
+            });
     };
 
     return (
         <>
-            {provider ? (
-                <PrimaryButton title="Disconnect" callback={handleDisconnect} />
-            ) : (
-                <ConnectButton title={title} icon={icon} callback={connect} />
-            )}
+            {uri}
+            <ConnectButton title={title} icon={icon} callback={connect} />
         </>
     );
 };
