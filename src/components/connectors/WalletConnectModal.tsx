adb55d8a4a52ae77acafdb58594fcd2de799aefd
daniel-vahn <d.vonahn@chainsulting.de>
2024-04-26 10:03:27 +0200

    Refactor WalletConnectModal component to use useWeb3Modal hook

--- src/components/connectors/WalletConnectModal.tsx
+++ src/components/connectors/WalletConnectModal.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-// import axios from 'axios';
-// import WebApp from '@twa-dev/sdk';
+import { useWeb3Modal } from '@web3modal/ethers/react';
 
 import ConnectButton from '../buttons/ConnectButton';
 
@@ -11,20 +10,10 @@ type Props = {
 };
 
 const WalletConnectModal: React.FC<Props> = ({ title, icon }) => {
-    // const [uri, setUri] = React.useState<string | null>(null);
+    const { open } = useWeb3Modal();
 
     const connect = async () => {
-        // Post request to localhost:3000/connect with axios
-        // axios
-        //     .post(
-        //         'https://8d58-2a02-8106-21-bc00-20a7-e089-bdb8-452a.ngrok-free.app/connect'
-        //     )
-        //     .then((response) => {
-        //         // Set uri with response data
-        //         console.log(response.data);
-        //         setUri(response.data.universalLink);
-        //         WebApp.openLink(response.data.universalLink);
-        //     });
+        open();
     };
 
     return (
