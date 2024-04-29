81c00233eb5f655f55f4eadddd6dfab278661187
daniel-vahn <d.vonahn@chainsulting.de>
2024-04-29 15:43:43 +0200

    Update WebApp.MainButton in App.tsx to enable and set color and text

--- src/main.tsx
+++ src/main.tsx
@@ -22,8 +22,7 @@ import './index.css';
 
 // Expand the Telegram Mini App to full screen
 WebApp.expand();
-WebApp.MainButton.disable();
-WebApp.MainButton.hide();
+
 // Initialize the Telegram Mini App SDK
 WebApp.ready();
 // Enable the closing confirmation
