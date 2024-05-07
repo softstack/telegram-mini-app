31d23755a7f3351b3afbf72a3064ab55971d8de2
daniel-vahn <d.vonahn@chainsulting.de>
2024-05-07 11:23:59 +0200

    refactor: Remove unused color-thief-node library import in NetworkBadge.tsx

--- src/components/connectOverlay/NetworkBadge.tsx
+++ src/components/connectOverlay/NetworkBadge.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { getColorFromURL } from 'color-thief-node';
+// import { getColorFromURL } from 'color-thief-node';
 
 type Props = {
     network: string;
@@ -15,17 +15,17 @@ const NetworkBadge: React.FC<Props> = ({
     selected,
     callback,
 }) => {
-    const getNetworkColor = async () => {
-        const color = await getColorFromURL(icon);
-        console.log(color);
-        return color;
-    };
+    // const getNetworkColor = async () => {
+    //     const color = await getColorFromURL(icon);
+    //     console.log(color);
+    //     return color;
+    // };
 
     return (
         <div
             onClick={callback}
             className={`flex flex-col items-center justify-center text-customBlackText rounded-lg p-2 ${
-                selected ? `bg-${getNetworkColor}` : ''
+                selected ? `bg-customBlueSelected` : ''
             }`}
         >
             <img className="w-11 h-11" src={icon} alt="" />
