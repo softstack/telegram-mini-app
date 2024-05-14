340335f4b57acb9745f437bd893ed2006f0073fa
daniel-vahn <d.vonahn@chainsulting.de>
2024-05-14 10:55:35 +0200

    chore: Add webpack build script to package.json

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/main.tsx', // Make sure this points to your entry file
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js', // This can be named anything; bundle.js is typical
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html', // Ensure you have an HTML template in your public folder
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            // You can add more loaders here for other types of files like CSS, SASS, etc.
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+};
