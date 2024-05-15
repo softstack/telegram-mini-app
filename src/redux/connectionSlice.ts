a3a24da1391cc7e9dbadc30d7cac4dd38c5158b1
daniel-vahn <d.vonahn@chainsulting.de>
2024-05-15 09:23:42 +0200

    refactor: Update connection state to 'disconnected' in connectionSlice.ts

--- src/redux/connectionSlice.ts
+++ src/redux/connectionSlice.ts
@@ -3,7 +3,7 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { ConnectionSliceState, ConnectionState } from '../types';
 
 const initialState: ConnectionSliceState = {
-    connectionState: 'connected',
+    connectionState: 'disconnected',
 };
 
 export const connectionSlice = createSlice({
