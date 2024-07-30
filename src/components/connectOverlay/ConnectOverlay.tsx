35ebf442ac68f650989bd069e9ccc631093ff5d4
daniel-vahn <d.vonahn@chainsulting.de>
2024-07-30 11:27:34 +0200

    debug

--- src/components/connectOverlay/ConnectOverlay.tsx
+++ src/components/connectOverlay/ConnectOverlay.tsx
@@ -98,6 +98,9 @@ const ConnectOverlay: React.FC<Props> = ({
             window.localStorage.removeItem('walletconnect');
             window.localStorage.removeItem('WALLETCONNECT_DEEPLINK_CHOICE');
 
+            console.log('Connecting Wallet...');
+            console.log('Bridge URL: ', BRIDGE_URL);
+
             const response = await axios.post(BRIDGE_URL + '/init-provider', {
                 wallet: wallet,
             });
